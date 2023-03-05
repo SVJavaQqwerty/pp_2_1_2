93c030ddac44148690c2a55b59df4f0c201c74c2
пк <mr.setevik@rambler.ru>
2023-03-05 10:59:26 +0300

    Result

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,11 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanSecond =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(beanSecond));
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat catSecond = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.equals(beanSecond));
     }
 }
